ed3177002d5b22cb8182beff28c0b1bf48080759
Aditya Chaudhary <adityachaudharyfb@gmail.com>
2015-07-25 19:40:31 +0530

    All models-serializers complete.

--- injector/serializers.py
+++ injector/serializers.py
@@ -3,7 +3,7 @@ __author__ = 'aditya'
 
 from django.forms import widgets
 from rest_framework import serializers
-from injector.models import Technology
+from injector.models import *
 
 
 class TechnologySerializer(serializers.ModelSerializer):
@@ -25,3 +25,50 @@ class TechnologySerializer(serializers.ModelSerializer):
                   'isMobile',
                   'javaEnabled',
                   'userAgent')
+
+
+
+class ReferrerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Referrer
+        fields = ('id',
+                  'referrerUrl')
+
+
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page
+        fields = ('id',
+                  'url',
+                  'pageTitle',
+                  'loadTime',
+                  'pageVisitTimeStamp')
+
+
+
+
+class GuidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guid
+        fields = ('id',
+                  'guid')
+
+
+class EventStorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventStory
+        fields = ('id',
+                  'guid',
+                  'page',
+                  'referrer',
+                  'technology')
+
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('id',
+                  'eventName',
+                  'eventStory')
